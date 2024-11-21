010d8b2dc3c3e6c8ffb18260e922cd7b85ced5a5
almunna <munna.kuet.me@gmail.com>
2024-11-22 00:48:15 +0600

    models created

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -2,6 +2,19 @@ import UserModel from "../models/userModel.js";
 
 async function registerUser(req, res) {
     try {
+        const  {name, email, password} = req.body;
+
+        if (!name || !email || !password){
+           return res.status(400).json({ message: "Provide name, email, password"
+    })}
+        const user = await UserModel.findOne({email})
+        if(user){
+            return res.json({
+                message: "Email already exist",
+                error: true,
+                success: false
+            })
+        }
 
     } catch (error) {
         return res.status(500).json({
